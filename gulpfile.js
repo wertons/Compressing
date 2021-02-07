ccac8727d4082670c6fb8abfc499673f77753e8a
wertons <paubestardsatorra@gmail.com>
2021-02-07 02:52:01 +0100

    MORE COMPRESSION

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 const { series, src, dest } = require('gulp');
 const cleanCSS = require('gulp-clean-css');
-const imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var imagemin = require('gulp-imagemin');
+var imageminPngquant = require('imagemin-pngquant');
+var imageminZopfli = require('imagemin-zopfli');
+var imageminMozjpeg = require('imagemin-mozjpeg'); 
+var imageminGiflossy = require('imagemin-giflossy');
 
 function clean(cb) {
     cb();
@@ -17,14 +22,46 @@ function minCSS() {
 
 }
 function minImg(){
-    return src('./img/*')
-    .pipe(imagemin())
-    .pipe(dest('./img'))
+    return src(['./img/**/*.{gif,png,jpg,svg}'])
+    .pipe(cache(imagemin([
+        //png
+        imageminPngquant({
+            speed: 1,
+            quality: [0.95, 1] //lossy settings
+        }),
+        imageminZopfli({
+            more: true
+            // iterations: 50 // very slow but more effective
+        }),
+        //gif
+        // imagemin.gifsicle({
+        //     interlaced: true,
+        //     optimizationLevel: 3
+        // }),
+        //gif very light lossy, use only one of gifsicle or Giflossy
+        imageminGiflossy({
+            optimizationLevel: 3,
+            optimize: 3, //keep-empty: Preserve empty transparent frames
+            lossy: 2
+        }),
+        //svg
+        imagemin.svgo({
+            plugins: [{
+                removeViewBox: false
+            }]
+        }),
+        //jpg lossless
+        imagemin.mozjpeg({
+            progressive: true
+        }),
+        //jpg very light lossy, use vs jpegtran
+        imageminMozjpeg({
+            quality: 90
+        })
+    ])))
+    .pipe(dest('./img'));
 }
 
 
 
-exports.build = build;
-exports.default = series(clean, build);
-exports.minifyCSS = minCSS;
 exports.compress = series(clean, build, minCSS, minImg);
