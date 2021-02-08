7def65e2f40e7133465faae9147ff639359f02b4
wertons <paubestardsatorra@gmail.com>
2021-02-08 22:58:24 +0100

    removed imgs from cache

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "globDirectory": "./",
   "globPatterns": [
-    "**/*.{css,scss,eot,ttf,woff,woff2,otf,js,jpg,webp,ico,png,html,json}"
+    "**/*.{css,scss,eot,ttf,woff,woff2,otf,js,jpg,ico,html,json}"
   ],
   "swDest": "sw.js"
-};
+}; //imgs have been removed from cache as they were far too heavy a data load for the pwa to handle
