b735092c76438cbb3424d00d6f1434270226cc9b
wertons <paubestardsatorra@gmail.com>
2021-02-08 23:01:04 +0100

    workbox error

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "globDirectory": "./",
   "globPatterns": [
-    "**/*.{css,scss,eot,ttf,woff,woff2,otf,js,jpg,ico,html,json}"
+    "**/*.{css,scss,eot,ttf,woff,woff2,otf,js,ico,html,json}"
   ],
   "swDest": "sw.js"
-}; //imgs have been removed from cache as they were far too heavy a data load for the pwa to handle
+};
